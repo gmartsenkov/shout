52c3325ff7f2f65ebd946cd74d26087be2533941
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-12-23 23:09:34 +0000

    improve how compile time subscriptions are stored

--- lib/shout.ex
+++ lib/shout.ex
@@ -2,26 +2,31 @@ defprotocol CompileTimeSubscribers do
   def compile_time_subscriptions(_)
 end
 
+defmodule Shout.Subs do
+  defmacro __before_compile__(_env) do
+    quote do
+      def compile_time_subscriptions() do
+        @compile_time_subscriptions
+      end
+    end
+  end
+end
+
 defmodule Shout do
   alias Shout.Subscription
 
   @compile_subscription_apps Application.compile_env(:shout, :apps, [])
 
   defmacro __using__(_env) do
-	  quote do
-      Module.register_attribute(__MODULE__, :compile_time_subscriptions, accumulate: true, persist: true)
+    quote do
+      Module.register_attribute(__MODULE__, :compile_time_subscriptions, accumulate: true)
 
       import Shout
 
-      defimpl CompileTimeSubscribers  do
-        def compile_time_subscriptions(_any), do: nil
-      end
+      @before_compile Shout.Subs
 
-      def compile_time_subscriptions() do
-        __MODULE__.module_info(:attributes)
-        |> Enum.map(fn {k,v} -> if k == :compile_time_subscriptions, do: v end)
-        |> Enum.reject(&is_nil/1)
-        |> List.flatten()
+      defimpl CompileTimeSubscribers do
+        def compile_time_subscriptions(_any), do: nil
       end
     end
   end
@@ -29,9 +34,11 @@ defmodule Shout do
   defmacro subscribe(from, event, to) do
     quote do
       subscription = %Subscription{from: unquote(from), event: unquote(event), to: unquote(to)}
+
       case __ENV__ do
         %{function: nil} ->
           Module.put_attribute(__MODULE__, :compile_time_subscriptions, subscription)
+
         _else ->
           Shout.Store.register_subscription(subscription)
       end
--- test/shout_test.exs
+++ test/shout_test.exs
@@ -1,12 +1,15 @@
 defmodule ShoutTest do
   use ExUnit.Case
-  doctest Shout
+  import Assertions, only: [assert_lists_equal: 2]
 
   test "compile_time_subscriptions" do
-    assert Shout.compile_time_subscriptions() == [
-      %Shout.Subscription{event: :flatten, from: List, to: &Enum.sort/1},
-      %Shout.Subscription{event: :some_event, from: Module, to: &String.split/2},
-      %Shout.Subscription{event: :another_event, from: Enum, to: &String.split/1}
-    ]
+    assert_lists_equal(
+      Shout.compile_time_subscriptions(),
+      [
+        %Shout.Subscription{event: :flatten, from: List, to: &Enum.sort/1},
+        %Shout.Subscription{event: :some_event, from: Module, to: &String.split/2},
+        %Shout.Subscription{event: :another_event, from: Enum, to: &String.split/1}
+      ]
+    )
   end
 end
